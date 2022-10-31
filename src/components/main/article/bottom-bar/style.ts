a9071fcad5208137e2a685e463a48252ffde67a6
miles <a09112332@gmail.com>
2022-11-01 01:42:17 +0800

    fix article style

--- src/components/main/article/bottom-bar/style.ts
+++ src/components/main/article/bottom-bar/style.ts
@@ -2,10 +2,11 @@ import styled from 'styled-components'
 
 export const BottomBarWrapper = styled.div`
   position: sticky;
-  top: 0px;
+  bottom: 0px;
   /* width: 100%; */
   height: 48px;
   bottom: 0;
   right: 0;
+  z-index: 99;
   background-color: red;
 `
